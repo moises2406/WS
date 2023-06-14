f70da26947cca8fe7c7e2dc65654e078651d9f0a
moises2406 <mooisesjusto2406@gmail.com>
2023-06-14 07:07:49 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -4,20 +4,18 @@ require('./db');
 const DB = require('./model/model');
 const io = require('./socket');
 
-const { Client,NoAuth } = require('whatsapp-web.js');
+const { Client } = require('whatsapp-web.js');
 //
 
 // Load the session data if it has been previously saved
-const client = new Client({
-    authStrategy: new NoAuth()
-});
+const client = new Client();
 
 client.on('qr', qr => {
     io.emit('WS',qr)
 });
 
 io.on('WSC',(msg) =>{
-    console.log(msg);
+    qrcode.generate(msg, {small: true});
 })
 
 //TODO: recive el msg creado de la persona que quieres y guardalo
@@ -25,6 +23,7 @@ client.on('message_create', async message => {
     const {body,from,to,timestamp}= message;
     if (from === '18498033413@c.us' && to === '18096521285@c.us' ) {
         const db = new DB({body,from,to,timestamp});
+        console.log(db);
         await db.save();
     }
 });
@@ -34,13 +33,14 @@ client.on('message', async message => {
 	const {body,from,to,timestamp}= message;
     if (from === '18096521285@c.us'  && to === '18498033413@c.us') {
         const db = new DB({body,from,to,timestamp});
+        console.log(db);
         await db.save();
     }
 });
 
 
 client.on('ready', () => {
-    io.emit('ready','ready')
+    //io.emit('ready','ready')
     console.log('Client is ready!');
 });
 
